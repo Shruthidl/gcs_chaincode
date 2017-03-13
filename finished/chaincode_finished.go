a48abda35e0ba5d9b6dad57359098fd97a691582
Shruthidl <shruthidl91@gmail.com>
2017-03-13 11:43:51 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -180,3 +180,27 @@ func (t *SimpleChaincode) addOutClearFile(stub shim.ChaincodeStubInterface, args
 	     return []byte("Invalid|20-01-2017 07:20AM"), nil;
     }
 
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	
+	fmt.Println("retrieving state for key: " + key);
+	
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+        //valAsbytes = []byte(valAsbytes);
+	return valAsbytes, nil
+}
+
